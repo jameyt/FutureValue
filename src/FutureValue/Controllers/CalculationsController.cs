6f3cb6f1b9a2ca05bbcd8eab02b9509c022d8e0d
Jamey Tyler <jameyerictyler@gmail.com>
2016-03-09 20:27:10 -0600

    init commit

---
+++ src/FutureValue/Controllers/CalculationsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace FutureValue.Controllers
+{
+    [Route("api/[controller]")]
+    public class CalculationsController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public double CalculateFutureValue(double principal,double rate, double years, double compounds)
+        {
+            return Math.Pow(principal*(1+rate/years),years*compounds);
+        }        
+    }
+}
