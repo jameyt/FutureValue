d3e8da79acbd72e8123f43de4f197456ea01955e
Jamey Tyler <jameyerictyler@gmail.com>
2016-03-12 11:12:45 -0600

    excluded files included

---
+++ src/FutureValueTests/CalculationTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FutureValueTests
+{
+    [TestClass]
+    public class CalculationTests
+    {
+        [TestMethod]
+        public void TestCompoundInterest1()
+        {
+        }
+    }
+}
